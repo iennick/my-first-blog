0e8fbfa58394c5ba28cf47f5bf273cce9e89f59c
iennick <iennicktv@gmail.com>
2022-05-09 17:11:36 +0300

    My Django Girls app, first commit

---
+++ First_test/pyton_intro.py
@@ -0,0 +1,12 @@
+#def hi(name):
+ #   print('Hi ' + name + '!')
+
+#girls = ['Kvitka', 'Natali', 'Anastasia', 'Your wife', 'Blody whore']
+#for name in girls:
+ #   hi(name)
+  #  print ('Next girl!')
+
+#hi()
+
+for i in range (1, 23):
+    print(i)
